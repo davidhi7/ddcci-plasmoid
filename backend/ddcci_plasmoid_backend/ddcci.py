5de995fb4e85e6c0d07d7c094ff0936cab427536
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-04-10 21:10:05 +0200

    issue #1: Fix identification of duplicate monitors

--- backend/ddcci_plasmoid_backend/ddcci.py
+++ backend/ddcci_plasmoid_backend/ddcci.py
@@ -16,6 +16,25 @@ class CommandOutput(TypedDict):
     stderr: str
 
 
+# Record for identifying a monitor by its `Serial number` as well as `Binary serial number` EDID value reported by
+# ddcutil to look for duplicate monitors. Issue #1 shows that comparing by Serial number only is not sufficient, as the
+# monitors of the reporter only reported a binary serial number.
+class MonitorID:
+    def __init__(self, serial_number: str, binary_serial_number: str):
+        self.serial_number = serial_number
+        self.binary_serial_number = binary_serial_number
+
+    def __eq__(self, other: 'MonitorID') -> bool:
+        # If either one serial number is missing or both are unequal, the monitors are not identical.
+        if not self.serial_number or not other.serial_number \
+                or self.serial_number != other.serial_number:
+            return False
+        if not self.binary_serial_number or not other.binary_serial_number or \
+                self.binary_serial_number != other.binary_serial_number:
+            return False
+        return True
+
+
 async def detect():
     async def fetch_monitor_data(node: Node):
         display_id = get_monitor_id(node)
@@ -23,7 +42,7 @@ async def detect():
         bus_id = int(re.search(r'\d+$', node.child_by_key['I2C bus'].value).group())
 
         cmd = f'ddcutil getvcp --bus {bus_id} --brief {brightness_feature_code:x}'
-        proc = await asyncio.subprocess.create_subprocess_shell( cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        proc = await asyncio.subprocess.create_subprocess_shell(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = await proc.communicate()
         if proc.returncode > 0:
             logger.debug(
@@ -42,21 +61,25 @@ async def detect():
     output = subprocess_wrapper('ddcutil detect')
     content = Node.parse_indented_text(output['stdout'].split('\n'))
 
-    found_serial_numbers = []
+    found_monitors: list[MonitorID] = []
     awaitables = []
     for child in content.children:
         if not re.fullmatch(r'Display \d+', child.key.strip()):
             continue
+
         # monitors connected to DisplayPort may appear twice. This is apparently related to DisplayPort MST.
         # Since the EDID data of both entries is identical, we simply remove duplicate monitors based on their serial
         # number
-        serial_number = get_EDID_value(child, 'Serial number')
-        if serial_number in found_serial_numbers:
+        monitorId = MonitorID(
+            serial_number=get_EDID_value(child, 'Serial number'),
+            binary_serial_number=get_EDID_value(child, 'Binary serial number')
+        )
+        if monitorId in found_monitors:
             logger.debug(
                 f'{get_EDID_value(child, "Model")} id={get_monitor_id(child)}: Duplicate monitor found and removed'
             )
             continue
-        found_serial_numbers.append(serial_number)
+        found_monitors.append(monitorId)
         awaitables.append(fetch_monitor_data(child))
 
     return await asyncio.gather(*awaitables, return_exceptions=True)
