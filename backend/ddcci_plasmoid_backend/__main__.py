88c7120844595eb63e7381a8da9c8b0e8c4ed111
David Himmelstoß <77309510+davidhi7@users.noreply.github.com>
2023-04-26 18:50:32 +0200

    Improve logging

--- backend/ddcci_plasmoid_backend/__main__.py
+++ backend/ddcci_plasmoid_backend/__main__.py
@@ -2,6 +2,7 @@ import asyncio
 import getpass
 import json
 import logging
+import re
 import subprocess
 import sys
 import tempfile
@@ -15,15 +16,43 @@ from ddcci_plasmoid_backend import ddcci
 from ddcci_plasmoid_backend.__init__ import get_parser
 
 
+def get_ddcutil_version() -> str:
+    result = subprocess.run(['ddcutil', '--version'], check=True, stdout=subprocess.PIPE)
+    # First line of ddcutil output contains the version
+    version_line = result.stdout.decode().split('\n')[0]
+    # Remove all characters from this line except for points followed by a number
+    # This is supposed to extract the version (x.y.z) from the string
+    return re.sub(r'(?!\.\d)\D', '', version_line)
+
+
 def main():
     arguments = vars(get_parser().parse_args())
 
-    logging.basicConfig(format='%(levelname)s %(name)s: %(message)s',
-                        level=logging.DEBUG if arguments['debug'] else logging.INFO)
+    logging_formatter = logging.Formatter('%(levelname)s %(name)s: %(message)s')
+
+    logging.getLogger().setLevel(logging.DEBUG)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(logging_formatter)
+    stream_handler.setLevel(logging.DEBUG if arguments['debug'] else logging.INFO)
+    logging.getLogger().addHandler(stream_handler)
+
+    if arguments['debug_log']:
+        log_path: Path = arguments['debug_log']
+        # Fail if log_path exists but is not a file
+        if not log_path.is_file() and log_path.exists():
+            logging.debug('`LOG_FILE` must be a valid file')
+        else:
+            logging.debug(f'Writing logs to file {log_path}')
+            file_handler = logging.FileHandler(log_path)
+            file_handler.setFormatter(logging_formatter)
+            file_handler.setLevel(logging.DEBUG)
+            logging.getLogger().addHandler(file_handler)
+
     # supress log message `DEBUG asyncio: Using selector: EpollSelector`
     logging.getLogger('asyncio').setLevel(logging.WARNING)
     logger = logging.getLogger(__name__)
-    logger.debug(f'Running version {version("ddcci-plasmoid-backend")} in debug mode')
+    logger.debug(f'backend version: {version("ddcci-plasmoid-backend")}')
+    logger.debug(f'ddcutil version: {get_ddcutil_version()}')
 
     def handle_error(error: str | subprocess.CalledProcessError) -> NoReturn:
         if isinstance(error, subprocess.CalledProcessError):
@@ -34,7 +63,7 @@ def main():
         }))
         sys.exit(1)
 
-    logger.debug(f'Command: {arguments["command"]}')
+    logger.debug(f'argv: {" ".join(sys.argv)}')
 
     if arguments['command'] == 'version':
         print(version('ddcci-plasmoid-backend'))
